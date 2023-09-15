878ae0c1b237312c93de4c23f76c99f6e812fa3f
aryansonid <aryan.s400029@gmail.com>
2023-09-15 11:29:18 +0530

    feat: added contracts

---
+++ contracts/Oracle.sol
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+contract Oracle {
+    struct Observation {
+        // the block timestamp of the last updation
+        uint256 lastUpdateblockTimestamp;
+        // the price accumelator
+        uint256 priceCumulative;
+        // the seconds cummelated for every price update
+        uint256 secondsPerLiquidityCumulative;
+        // initialize time
+        uint256 initializeTime;
+    }
+
+    mapping(address => Observation) priceData;
+
+    function transformPriceData(
+        Observation memory last,
+        uint256 blockTimestamp,
+        uint256 price
+    ) private pure returns (Observation memory) {
+        uint256 delta = blockTimestamp - last.lastUpdateblockTimestamp;
+        return
+            Observation({
+                lastUpdateblockTimestamp: blockTimestamp,
+                priceCumulative: last.priceCumulative + (price * delta),
+                secondsPerLiquidityCumulative: last
+                    .secondsPerLiquidityCumulative + delta,
+                initializeTime: last.initializeTime
+            });
+    }
+
+    function initializePriceData(
+        uint256 blockTimestamp,
+        uint256 price
+    ) private pure returns (Observation memory) {
+        return
+            Observation({
+                lastUpdateblockTimestamp: blockTimestamp,
+                priceCumulative: price,
+                secondsPerLiquidityCumulative: 1,
+                initializeTime: blockTimestamp
+            });
+    }
+
+    function feedPrice(address tokenAddress, uint256 price) public {
+        Observation memory last = priceData[tokenAddress];
+        uint256 initializatonDelta = block.timestamp - last.initializeTime;
+        Observation memory upadate;
+        if (
+            initializatonDelta < 8 hours &&
+            !(initializatonDelta == block.timestamp)
+        ) {
+            upadate = transformPriceData(last, block.timestamp, price);
+        } else {
+            upadate = initializePriceData(block.timestamp, price);
+        }
+        priceData[tokenAddress] = upadate;
+    }
+
+    function feedPriceArray(
+        address[] calldata tokenAddress,
+        uint256[] calldata prices
+    ) external {
+        for (uint256 i; i < tokenAddress.length; i++) {
+            feedPrice(tokenAddress[i], prices[i]);
+        }
+    }
+
+    function getTWAP(
+        address tokenAddress
+    ) external view returns (uint256 twapPrice) {
+        Observation memory tokenPriceData = priceData[tokenAddress];
+        twapPrice =
+            tokenPriceData.priceCumulative /
+            tokenPriceData.secondsPerLiquidityCumulative;
+    }
+}
--- contracts/mocks/MockGToken.sol
+++ contracts/mocks/MockGToken.sol
@@ -9,6 +9,4 @@ contract MockGtoken is ERC20 {
         string memory name_,
         string memory symbol_
     ) ERC20(name_, symbol_) {}
-
-    // add Roles
 }
