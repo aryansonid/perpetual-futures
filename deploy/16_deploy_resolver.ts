39b73be40048ac08dbdfb21e7ba4ff0312d6e241
aryansonid <aryan.s400029@gmail.com>
2023-11-07 21:01:37 +0530

    feat: updated and added deployment script

---
+++ deploy/16_deploy_resolver.ts
@@ -0,0 +1,19 @@
+import { DeployFunction } from "hardhat-deploy/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts, ethers } = hre;
+  const { deploy } = deployments;
+  const { deployer, priceSetter } = await getNamedAccounts();
+  const Storage = await deployments.get("Storage");
+
+  await deploy("Resolver", {
+    from: deployer,
+    contract: "Resolver",
+    args: [Storage.address],
+    log: true,
+  });
+};
+
+export default func;
+func.tags = ["oracle"];
